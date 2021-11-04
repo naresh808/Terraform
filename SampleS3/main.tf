ff53c2165540395b6e981ec2ba710ba41df97282
naresh808 <84272919+naresh808@users.noreply.github.com>
2021-11-04 11:13:31 +0530

    Add files via upload

---
+++ SampleS3/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.63.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "ap-south-1"
+}
+
+# Resource for S3
+resource "aws_s3_bucket" "challa" {
+    bucket = "adichalla"
+  
+}
+	
